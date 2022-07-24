3b6d0281dfb13b54ae4910234b5eb3deb35f1fbe
yudong <nanjing1222@163.com>
2022-07-25 00:00:37 +0800

    add json

--- test_json.go
+++ test_json.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Movie struct {
+	Title  string   `json:"title"`
+	Year   int      `json:"year"`
+	Price  int      `json:"rmb"`
+	Actors []string `json:"actors"`
+}
+
+func main() {
+	movie := Movie{"喜剧之王", 2000, 10, []string{"Rose", "Jack"}}
+
+	// struct -> json
+	jsonStr, err := json.Marshal(movie)
+	if err != nil {
+		fmt.Println("json marshal error", err)
+		return
+	}
+	fmt.Printf("jsonStr = %s\n", jsonStr)
+
+	//json -> struct
+	myMovie := Movie{}
+	err = json.Unmarshal(jsonStr, &myMovie)
+	if err != nil {
+		fmt.Println("json unmarshal error", err)
+		return
+	}
+	fmt.Printf("myMove is %v\n", myMovie)
+
+}
